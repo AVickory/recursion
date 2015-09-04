001015eefebab041ff022e97f57ae9c59b997557
AVickory <andrew.vickory@gmail.com>
2015-09-04 11:12:36 -0700

    I think I completed getElementsByClassName.  It passes the test, but I think I may try to do some of the lower level stuff manually.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+
+
+var getElementsByClassName = function(className) {
+  var nodeList = [];
+
+  var classes = className.split(' ');
+  classLen = classes.length;
+
+  var hasClasses = function (node) {
+    for(var i = 0; i < classLen; i++) {
+      if(!node.classList.contains(classes[i])) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var gebcn = function (node) {
+    if(node.nodeType === 1) {
+      if(hasClasses(node)) {
+        nodeList.push(node);
+      }
+      var children = node.childNodes;
+      var childrenLen = children.length;
+      //this loop is effectively my base case because it prevents recursion when there are no children.
+      for(var i = 0; i < childrenLen; i++) {
+        gebcn(children[i]);
+      }
+    }
+  };
+
+  gebcn(document.body)
+
+  return nodeList
+
 };
